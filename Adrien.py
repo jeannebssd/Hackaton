2e9fd4f25b4ffe961517c74ffeede44418b466f5
21Vania <vania.clesca@mines-paristech.fr>
2022-01-28 14:13:51 +0100

    Update Adrien.py

--- Adrien.py
+++ Adrien.py
@@ -4,43 +4,36 @@ import random as rd
 import pygame as pg
 from itertools import product
 
-def number_room():
-    k = rd.randint(2,10)   # on considère que l'on a a minima 2 salles
+# def number_room():
+#     k = rd.randint(2,10)   # on considère que l'on a a minima 2 salles
 
 
-def genere_salle():
-    point, L, l = (-1,2), 4, 5     #room_information()
-    M = np.array(L*[l*["."]])      # M est la matrice représentant la salle en question
-                                   # elle est initialement pleine de points
+# def genere_salle():
+#     point, L, l = (-1,2), 4, 5     #room_information()
+#     M = np.array(L*[l*["."]])      # M est la matrice représentant la salle en question
+#                                    # elle est initialement pleine de points
 
-    # génération des murs
+#     # génération des murs
 
-    for i in range(L-1):
-        M[i][0] = '|'
-        M[i,-1] = '|'
-    for j in range(l):
-        M[0,j] = '-'
-        M[-1,j] = '-'
+#     for i in range(L-1):
+#         M[i][0] = '|'
+#         M[i,-1] = '|'
+#     for j in range(l):
+#         M[0,j] = '-'
+#         M[-1,j] = '-'
 
-    return M
+#     return M
 
-print(genere_salle())
-
-DIRECTIONS = {
-    "DOWN": (0, +1),
-    "UP": (0, -1),
-    "RIGHT": (+1, 0),
-    "LEFT": (-1, 0),
-}
-
-direction = DIRECTIONS["RIGHT"]
+# print(genere_salle())
 
 CHARACTER_COLOR = (128, 128, 0)
-DIRECTIONS = {"DOWN": (0, -1), "UP": (0, +1), "RIGHT": (+1, 0), "LEFT": (-1, 0)}
+DIRECTIONS = {"DOWN": (0, 1), "UP": (0, -1), "RIGHT": (+1, 0), "LEFT": (-1, 0)}
 W = 10
 H = 10
 X = 40
 Y = 40
+
+direction = DIRECTIONS["RIGHT"]
 
 def draw_tile(x, y, color):
     """
@@ -54,27 +47,29 @@ pg.init()
 screen = pg.display.set_mode((X * W, Y * H))
 clock = pg.time.Clock()
 
-character_initial = (10, 15)
-
 def move(character, direction):
     x, y = character
     dx, dy = direction
     return (x + dx, y + dy)
 
 WHITE = (240, 240, 240)
-BLACK = (255, 255, 255)
+BLACK = (0, 0, 0)
 def draw_background():
-    screen.fill(WHITE)
-    for x in range(X):
-        for y in range(Y):
-            if (x + y) % 2 == 0:
-                draw_tile(x, y, BLACK)
+    screen.fill(BLACK)
 
+
+character_initial = (10, 15)
 character = character_initial
+
+murs = []
+for i in rooms:
+    for j in pourtour(i):
+        murs.append(i)
+
 running = True
 while running:
 
-    clock.tick(5)
+    clock.tick(15)
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -90,7 +85,16 @@ while running:
                 direction = DIRECTIONS["LEFT"]
             elif event.key == pg.K_q:
                 running = False
-
+    
+    new_character = move(character, direction)
+    if new_character in murs:
+        if new_character not in corridor:
+            direction = (0, 0)
+            print("Interdit de foncer dans le mur")
+    elif character in corridor:
+        if new_character not in (corridor[corridor.index(character)-1], corridor[corridor.index(character)+1]):
+            direction = (0, 0)
+            print("Interdit de quitter le couloir")
     character = move(character, direction)
     draw_background()
     draw_tile(character[0], character[1], CHARACTER_COLOR)
